d7d119a51b1718e4b6919b01940e8f01df4b0d10
oliwierptak <oliwierptak@gmail.com>
2013-05-10 18:19:19 +0200

    added setters to Environment

--- Src/Everon/Environment.php
+++ Src/Everon/Environment.php
@@ -52,20 +52,40 @@ class Environment implements Interfaces\Environment
     {
         return $this->resources['root'];
     }
+    
+    public function setRoot($root)
+    {
+        $this->resources['root'] = $root;
+    }
 
     public function getConfig()
     {
         return $this->resources['config'];
+    }
+    
+    public function setConfig($config)
+    {
+        $this->resources['config'] = $config;
     }
 
     public function getModel()
     {
         return $this->resources['model'];
     }
+    
+    public function setModel($model)
+    {
+        $this->resources['model'] = $model;
+    }
 
     public function getView()
     {
         return $this->resources['view'];
+    }
+    
+    public function setView($view)
+    {
+        $this->resources['view'] = $view;
     }
 
     public function getViewTemplate()
@@ -73,53 +93,108 @@ class Environment implements Interfaces\Environment
         return $this->resources['view_template'];
     }
 
+    public function setViewTemplate($view_template)
+    {
+        $this->resources['view_template'] = $view_template;
+    }
+
     public function getController()
     {
         return $this->resources['controller'];
+    }
+
+    public function setController($controller)
+    {
+        $this->resources['controller'] = $controller;
     }
 
     public function getSource()
     {
         return $this->resources['source'];
     }
+    
+    public function setSource($source)
+    {
+        $this->resources['source'] = $source;
+    }
 
     public function getTest()
     {
         return $this->resources['tests'];
+    }
+    
+    public function setTest($test)
+    {
+        $this->resources['tests'] = $test;
     }
 
     public function getEveron()
     {
         return $this->resources['everon'];
     }
+    
+    public function setEveron($everon)
+    {
+        $this->resources['everon'] = $everon;
+    }
 
     public function getEveronLib()
     {
         return $this->resources['everon_lib'];
+    }
+    
+    public function setEveronLib($everon_list)
+    {
+        $this->resources['everon_lib'] = $everon_list;
     }
 
     public function getEveronInterface()
     {
         return $this->resources['everon_interface'];
     }
+    
+    public function setEveronInterface($everon_interfaces)
+    {
+        $this->resources['everon_interface'] = $$everon_interfaces;
+    }
 
     public function getTmp()
     {
         return $this->resources['tmp'];
+    }
+    
+    public function setTmp($tmp)
+    {
+        $this->resources['tmp'] = $tmp;
     }
 
     public function getCache()
     {
         return $this->resources['cache'];
     }
+    
+    public function setCache($cache)
+    {
+        $this->resources['cache'] = $cache;
+    }
 
     public function getCacheConfig()
     {
         return $this->resources['cache_config'];
     }
+    
+    public function setCacheConfig($cache_config)
+    {
+        $this->resources['cache_config'] = $cache_config;
+    }
 
     public function getLog()
     {
         return $this->resources['log'];
+    }
+    
+    public function setLog($log)
+    {
+        $this->resources['log'] = $log;
     }
 }
--- Src/Everon/Interfaces/Environment.php
+++ Src/Everon/Interfaces/Environment.php
@@ -4,18 +4,33 @@ namespace Everon\Interfaces;
 interface Environment
 {
     function getRoot();
+    function setRoot($root);
     function getConfig();
+    function setConfig($config);
     function getModel();
+    function setModel($model);
     function getView();
+    function setView($view);
     function getViewTemplate();
+    function setViewTemplate($view_template);
     function getController();
+    function setController($controller);
     function getSource();
+    function setSource($source);
     function getTest();
+    function setTest($test);
     function getEveron();
+    function setEveron($everon);
     function getEveronLib();
+    function setEveronLib($everon_list);
     function getEveronInterface();
+    function setEveronInterface($everon_interfaces);
     function getTmp();
+    function setTmp($tmp);
     function getCache();
+    function setCache($cache);
     function getCacheConfig();
+    function setCacheConfig($cache_config);
     function getLog();
+    function setLog($log);
 }
