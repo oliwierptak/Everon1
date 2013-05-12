ac4a59fa74f1d895148bf2336ba76e0cb7ee1d4a
oliwierptak <oliwierptak@gmail.com>
2013-05-12 21:52:09 +0200

    updated phpdoc copyright headers

--- Web/index.php
+++ Web/index.php
@@ -1,8 +1,12 @@
 <?php
-/*
- * Everon
- * (c) 2011-2013 Oliwier Ptak
- * */
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Everon
 {
