27dc2c2e10e9a2347de4b7954129e6cbedc16ef5
oliwierptak <oliwierptak@gmail.com>
2013-05-14 00:42:07 +0200

    comment

--- Web/index.php
+++ Web/index.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * This file is part of the Everon framework.
- *
- * (c) Oliwier Ptak <oliwierptak@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * Everon application example.
  */
 
 namespace Everon
