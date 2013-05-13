637484f139da5e8006c6398a7c8805ae9bd1b74b
oliwierptak <oliwierptak@gmail.com>
2013-05-13 21:16:26 +0200

    updated _mpr()

--- Src/Everon/Lib/General.php
+++ Src/Everon/Lib/General.php
@@ -22,7 +22,7 @@ function mpr()
  * @param bool $die
  * @param int $traceIndex
  */
-function _mpr($val, $die=false, $traceIndex=0) {
+function _mpr($val, $die=false) {
     $is_pre = true;
     if (php_sapi_name() == 'cli') {
         $is_pre = false;
@@ -46,17 +46,7 @@ function _mpr($val, $die=false, $traceIndex=0) {
         echo '</pre>';
     }
 
-
     if ($die) {
-        $trace = debug_backtrace();
-        echo "--\n";
-        echo sprintf('Who called me: %s:%s', $trace[0]['file'], $trace[0]['line']);
-        if ($traceIndex) {
-            echo "\nTrace:";
-            for ($i=1; $i<=$traceIndex; $i++) {
-                echo sprintf("\n%s:%s", $trace[$i]['file'], $trace[$i]['line']);
-            }
-        }
         die();
     }
 }
