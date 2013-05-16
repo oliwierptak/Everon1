d451116bf5bc8177c8eddab25cf96fd805b77d82
oliwierptak <oliwierptak@gmail.com>
2013-05-16 22:54:45 +0200

    added Config\Item interface

---
+++ Src/Everon/Interfaces/ConfigItem.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Interfaces;
+
+
+interface ConfigItem
+{
+    function getName();
+    function setName($name);
+    function isDefault();
+    function setIsDefault($is_default);
+}
