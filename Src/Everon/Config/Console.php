4b286e523839f74ff69376ad209c4a5c0cc8b4cd
oliwierptak <oliwierptak@gmail.com>
2013-08-17 01:34:16 +0200

    added console routing config

---
+++ Src/Everon/Config/Console.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Config;
+
+use Everon\Interfaces;
+
+class Console extends \Everon\Config\Router implements Interfaces\Config
+{
+
+}
