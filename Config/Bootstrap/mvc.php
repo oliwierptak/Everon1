a9e51bb9973f87435db9863f9dc6a9c4aa82d114
oliwierptak <oliwierptak@gmail.com>
2013-11-06 03:49:56 +0100

    cleanups

--- Config/Bootstrap/mvc.php
+++ Config/Bootstrap/mvc.php
@@ -17,4 +17,14 @@ require_once($BootstrapFile);
 $Bootstrap->getClassLoader()->add('Everon\Model', $Environment->getModel());
 $Bootstrap->getClassLoader()->add('Everon\Mvc\Controller', $Environment->getController());
 
+//replace default Router
+$Container->register('Router', function() use ($Factory) {
+    $RouteConfig = $Factory->getDependencyContainer()->resolve('ConfigManager')->getRouterConfig();
+    $RouterValidator = $Factory->buildRouterValidator();
+    return $Factory->buildRouter($RouteConfig, $RouterValidator);
+});
 
+//replace default Request
+$Container->register('Request', function() use ($Factory) {
+    return $Factory->buildRequest($_SERVER, $_GET, $_POST, $_FILES);
+});
--- Web/index.php
+++ Web/index.php
@@ -4,19 +4,8 @@
  */
 namespace Everon;
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
-ini_set('xdebug.overload_var_dump', false);
-ini_set('html_errors', false);
-
+error_reporting(E_ALL);
 echo ('Start: '.memory_get_usage(TRUE)/1024)." kb<hr/>";
-
-
-/**
- * @var Interfaces\DependencyContainer $Container
- * @var Interfaces\Factory $Factory
- * @var Interfaces\Core $Mvc
- */
 
 $BootstrapFile = new \SplFileInfo(
     implode(DIRECTORY_SEPARATOR,
@@ -25,5 +14,4 @@ $BootstrapFile = new \SplFileInfo(
 require_once($BootstrapFile);
 
 $Application = $Factory->buildMvc();
-
 $Application->run($Guid);
