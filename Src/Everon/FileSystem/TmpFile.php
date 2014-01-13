dc8288597f4e853684816907db0e3eb0fc9e3757
oliwierptak <oliwierptak@gmail.com>
2014-01-13 13:11:14 +0100

    added FileSystem\TmpFile

---
+++ Src/Everon/FileSystem/TmpFile.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\FileSystem;
+
+use Everon\Dependency;
+use Everon\Exception;
+use Everon\Interfaces;
+
+class TmpFile implements Interfaces\FileSystemTmpFile
+{
+    protected $handle = null;
+    
+    
+    public function __construct()
+    {
+        $this->handle = tmpfile();
+    }
+    
+    public function write($content)
+    {
+        fwrite($this->handle, $content);
+    }
+    
+    public function getFilename()
+    {
+        $meta = stream_get_meta_data($this->handle);
+        return $meta['uri'];
+    }
+    
+    public function close()
+    {
+        if ($this->handle !== null) {
+            fclose($this->handle);
+            $this->handle = null;
+        }
+    }
+    
+    public function __destruct()
+    {
+        $this->close();
+    }
+}
