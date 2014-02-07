455e74fc489cbf3f8ca870bbdccaf77a1611cbe3
oliwierptak <oliwierptak@gmail.com>
2014-02-07 23:55:28 +0100

    removed tests and tmp

--- Tests/Everon/bootstrap_mvc.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Everon;
-
-$CustomSetup = function(){}; //disable default exception handler
-
-require_once(
-    implode(DIRECTORY_SEPARATOR,
-        [dirname(__FILE__), '..', '..', 'Config', 'Bootstrap', 'mvc.php'])  
-);
