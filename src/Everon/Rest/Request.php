e0dd622ee646630491e144b09dfeeaa3dc8e7859
oliwierptak <oliwierptak@gmail.com>
2014-02-17 15:15:20 +0100

    cleanups

--- src/Everon/Rest/Request.php
+++ src/Everon/Rest/Request.php
@@ -19,20 +19,4 @@ class Request extends \Everon\Request implements Interfaces\Request
     const METHOD_POST = 'POST';
     const METHOD_PUT = 'PUT';
 
-    /**
-     * @param array $server $_SERVER
-     * @param array $get $_GET
-     * @param array $post $_POST
-     * @param array $files $_FILES
-     */
-    public function __construct22(array $server, array $get, array $post, array $files)
-    {
-        $data = json_decode(file_get_contents('php://input'), true);
-        sd($data, $_POST, $_SERVER);
-        
-        parent::__construct($server, $get, $post, $files);
-        $this->initRequest();
-
-    }
-    
 }
