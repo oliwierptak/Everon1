1cfa3c59e01ad17dad40d83973beb6551506dffe
oliwierptak <oliwierptak@gmail.com>
2014-02-19 01:08:02 +0100

    cleanups

---
+++ src/Everon/Rest/Resource.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\Rest;
+
+
+class Resource implements Interfaces\Resource
+{
+    protected $name = null;
+
+    /**
+     * @var array
+     */
+    protected $data = null;
+    
+    
+    public function __construct($name, array $data)
+    {
+        $this->name = $name;
+        $this->data = $data;
+    }
+}
