8ffa944007350e9e5861234260b129e167335f02
oliwierptak <oliwierptak@gmail.com>
2014-03-31 11:29:38 +0200

    added Schema Table dependency

---
+++ src/Everon/DataMapper/Dependency/Table.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Everon\DataMapper\Dependency;
+
+
+trait Table
+{
+    /**
+     * @var \Everon\DataMapper\Interfaces\Schema\Table
+     */
+    protected $Table = null;
+
+
+    /**
+     * @return \Everon\DataMapper\Interfaces\Schema\Table
+     */
+    public function getSchema()
+    {
+        return $this->Table;
+    }
+
+    /**
+     * @param \Everon\DataMapper\Interfaces\Schema\Table
+     */
+    public function setTable(\Everon\DataMapper\Interfaces\Schema\Table $Table)
+    {
+        $this->Table = $Table;
+    }
+}
