5228e8e5e74547448e5f37f7e89d2ad8b4ab73d7
zeger015 <zeger_hoogeboom@hotmail.com>
2014-04-22 22:50:48 +0200

    Started email thing

---
+++ src/Everon/Email/Senders/PHPMailer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the Everon framework.
+ *
+ * (c) Oliwier Ptak <oliwierptak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Everon\Email\Send;
+
+
+use Everon\Email\Interfaces\Email;
+use Everon\Email\Interfaces\Sender;
+
+/**
+ * @author Zeger Hoogeboom <zeger_hoogeboom@hotmail.com>
+ */
+class PHPMailer implements Sender{
+
+    /**
+     * @inheritdoc
+     */
+    function send(Email $Email, $receiver)
+    {
+        mail($receiver,$Email->getSubject(),$Email->getMessage());
+    }
+
+} 
