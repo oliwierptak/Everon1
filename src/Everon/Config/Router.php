f8736a8c206348ebe8b7fc3c5b2abc9f92bedac2
oliwierptak <oliwierptak@gmail.com>
2014-05-21 10:44:21 +0200

    tmp fix

--- src/Everon/Config/Router.php
+++ src/Everon/Config/Router.php
@@ -16,7 +16,7 @@ class Router extends \Everon\Config
 {      
     protected function buildItem($name, array $data)
     {
-        $data['url'] = '%application.url.app%'.$data['url']; //auto append application url
+        $data['url'] = '/'.$data['url']; //auto append application url
         $data = $this->recompile($data);
         return $this->getFactory()->buildConfigItem($name, $data, 'Everon\Config\Item\Router');
     }
