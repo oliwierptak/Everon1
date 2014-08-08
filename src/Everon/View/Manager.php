f30717c4e552638d0b8b361dd3aff4fe901ea311
oliwierptak <oliwierptak@gmail.com>
2014-08-08 13:53:29 +0200

    views cleanups

--- src/Everon/View/Manager.php
+++ src/Everon/View/Manager.php
@@ -137,6 +137,7 @@ class Manager implements Interfaces\Manager
             $Layout = $makeLayout($name);
         }
         catch (Exception $e) {
+            $this->getLogger()->error($e);
             $Layout = $makeLayout($default_view);
         }
         
