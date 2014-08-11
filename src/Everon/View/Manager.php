b03b16826ce422396211f336c50e16199a176391
oliwierptak <oliwierptak@gmail.com>
2014-08-11 22:02:56 +0200

    error -> warning

--- src/Everon/View/Manager.php
+++ src/Everon/View/Manager.php
@@ -137,7 +137,7 @@ class Manager implements Interfaces\Manager
             $Layout = $makeLayout($name);
         }
         catch (Exception $e) {
-            $this->getLogger()->error($e);
+            $this->getLogger()->warning($e);
             $Layout = $makeLayout($default_view);
         }
         
