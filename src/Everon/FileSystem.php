aee949e23769051eaaf062c45af6c9497787c3c3
marcel <marcel@grofas.com>
2014-09-22 14:23:41 +0200

    cleanups

--- src/Everon/FileSystem.php
+++ src/Everon/FileSystem.php
@@ -92,7 +92,6 @@ class FileSystem implements Interfaces\FileSystem
         if ((new \SplFileInfo($root))->isDir() === false) {
             throw new Exception\FileSystem('Root directory does not exist: "%s"', $root);
         }
-
         $this->root = $Dir->getPathname().DIRECTORY_SEPARATOR;
     }
 
